42d345f2b6e9535a3026437f5bc064f123b24723
JP <email>
2023-06-05 22:17:57 +1000

    add playwright.config.js file

--- tests/failed-authorization.test.js
+++ tests/failed-authorization.test.js
@@ -1,17 +1,10 @@
-const { chromium } = require("playwright");
 const { test, expect } = require('@playwright/test');
 
 const email = "test@test.com";
 const password = "asd";
 
-test('failed login form authorization', async () => {
-    const browser = await chromium.launch({
-      headless: false,
-      slowMo: 100,
-      devtools: true
-    });
-  
-    const page = await browser.newPage();
+test('failed login form authorization', async ({ page }) => {
+
     await page.goto("https://netology.ru/?modal=sign_in", {setTimeout: 10000});
   
     await page.fill('[placeholder="Email"]', email);
@@ -22,6 +15,5 @@ test('failed login form authorization', async () => {
     const locator = page.locator("[data-testid='login-error-hint']");
     await expect(locator).toContainText("Вы ввели неправильно логин или пароль");
   
-    await browser.close();
   });
   
--- tests/successful-authorization.test.js
+++ tests/successful-authorization.test.js
@@ -1,4 +1,3 @@
-const { chromium } = require("playwright");
 const { test, expect } = require('@playwright/test');
 const user = require("../user.js");
 
@@ -6,23 +5,17 @@ const user = require("../user.js");
 const email = user.email;
 const password = user.password;
 
-test('successful login form authorization', async () => {
-  const browser = await chromium.launch({
-    headless: false,
-    slowMo: 100,
-    devtools: true
-  });
+test('successful login form authorization', async ({ page }) => {
 
-  const page = await browser.newPage();
   await page.goto("https://netology.ru/?modal=sign_in", {setTimeout: 10000});
 
+  page.pause();
   await page.fill('[placeholder="Email"]', email);
   await page.fill('[placeholder="Пароль"]', password);
 
   await page.click("button[data-testid='login-submit-btn']");
 
   const locator = page.locator('h2');
-  await expect(locator).toContainText("Мои курсы и профессии");
+  await expect(locator).toContainText("Мои курсы и профессии",{timeout: 10000});
 
-  await browser.close();
 });
